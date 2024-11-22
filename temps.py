9c6fd342d747ce83542a7f9b548d1a7c02f7c9d4
CWGrayson <cwgrayso@asu.edu>
2024-11-21 23:03:29 -0700

    Update temps.py
    Added in doc strings, clarified the origin of the 1.8 in a comment

--- temps.py
+++ temps.py
@@ -1,8 +1,19 @@
 NORMAL_LOW = 97  # normal human body temp lower end
 NORMAL_HIGH = 99  # normal human body temp higher end
-
+"""Creates the cut off threasholds for normal human body tempratures """
 
 def sort_temperatures(measurements):
+    """
+    Inputs:
+    Array of real numbers
+    Outputs:
+    3 arrays, low medium and high 
+    containing the input arrays values sorted acording to how they
+    fall with respect to the normal range of human body tempratures
+
+    Sorting is carried out by an if elif statment to identify thse above, and bellow the normal and
+    sorts remaning into the normal temprature array
+    """
     low = []
     normal = []
     high = []
@@ -19,13 +30,25 @@ def sort_temperatures(measurements):
 
 
 def convert_from_fahrenheit(temps):
-    results = []
+    """
+    Inputs: array of real numbers representing temprature in Deg F
+    Output: array of real numbers converted from Deg F to Deg C
+
+    Method (Temprature -32) divided by 1.8
+    """
+    results = [] 
     for temp in temps:
-        results.append((temp-32)/1.8)
+        results.append((temp-32)/1.8) #Each deg C increment is 9/5 of a deg F thus the division by 1.8
 
     return results
 
 def convert_to_fahrenheit(temps):
+        """
+    Inputs: array of real numbers representing temprature in Deg C
+    Output: array of real numbers converted from Deg C to Deg F
+
+    Method Temprature*1.8 +32
+    """
     results = []
     for temp in temps:
         results.append(temp*1.8+32)
